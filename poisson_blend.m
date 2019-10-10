0f449a2598b2588c1dc55b5c101507c353a31acf
Gavin-Yi <33049905+Gavin-Yi@users.noreply.github.com>
2019-10-10 09:46:31 +0800

    Update poisson_blend.m

--- poisson_blend.m
+++ poisson_blend.m
@@ -29,7 +29,7 @@ if nb == 3
 end
 
 %TODO: initialize counter, A (sparse matrix) and b.
-%Note: A don't have to be k°¡k,
+%Note: A don't have to be k√ók,
 %      you can add useless variables for convenience,
 %      e.g., a total of imh*imw variables
 i = zeros(1);  
@@ -37,90 +37,32 @@ j = zeros(1);
 v = zeros(1); 
 
 %TODO: fill the elements in A and b, for each pixel in the image
-counter = 1;
-row = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if mask_s( (n-1)*imh+m )  == 1
-            i(row) = counter;
-            j(row) = (n-1)*imh+m;
-            v(row) = 4;
-            counter = counter + 1;
-            row = row + 1;
-        end
-    end
-end
-counter = 1;
-row = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if n==1 || n==imw || m==1 || m== imh
-            i(row) = counter;
-            j(row) = (n-1)*imh+m;
-            v(row) = 1;
-        else
-            i(row) = counter;
-            j(row) = (n-1)*imh+m;
-            v(row) = 4;
-        end
-        counter = counter + 1;
-        row = row + 1;
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if n==1 || n==imw || m==1 || m== imh 
-        else
-            i(row) = counter;
-            j(row) = (n-1)*imh+m+1;
-            v(row) = -1;
-            row = row + 1;
-        end
-        counter = counter + 1;
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if n==1 || n==imw || m==1 || m== imh 
-        else
-            i(row) = counter;
-            j(row) = (n-1)*imh+m-1;
-            v(row) = -1;
-            row = row + 1;
-        end
-        counter = counter + 1;
-
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if n==1 || n==imw || m==1 || m== imh 
-        else
-            i(row) = counter;
-            j(row) = (n-2)*imh+m;
-            v(row) = -1;
-            row = row + 1;
-        end
-        counter = counter + 1;
-
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if n==1 || n==imw || m==1 || m== imh 
-        else
-            i(row) = counter;
-            j(row) = (n)*imh+m;
-            v(row) = -1;
-            row = row + 1;
-        end
-        counter = counter + 1;
-    end
-end
+% Inner
+n = 1:imh*imw;
+i(n) = 1:imw*imh;
+j(n) = 1:imw*imh;
+v(n) = 1 + 1*( (1<n)&(n<imh) | ((imw-1)*imh<n)&(n<imw*imh) | (mod(n,imh)==1&n~=1&n~=((imw-1)*imh+1)) | mod(n,imh)==0&n~=imh&n~=(imw*imh) )  ...
+    + 3*( ~( (1<=n)&(n<=imh) | ((imw-1)*imh<=n)&(n<=imw*imh) | (mod(n,imh)==1&n~=1&n~=((imw-1)*imh+1)) | mod(n,imh)==0&n~=imh&n~=(imw*imh) ) ) ;
+% top
+n = (imh*imw+1):2*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = (n - (imw*imh)-1 ).*( (1<mod(n - (imw*imh),imh)) )+1*(~( (1<mod(n - (imw*imh),imh)) ));
+v(n) = -1 * ( (1<mod(n - (imw*imh),imh)) );
+% bottom
+n = (2*imh*imw+1):3*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = (n - (2*imw*imh)+1 ).*( 1<(mod(n - (2*imw*imh),imh)) )+1*(~( 1<(mod(n - (2*imw*imh),imh)) ));
+v(n) = -1 * ( 1<(mod(n - (2*imw*imh),imh)) );
+% left
+n = (3*imh*imw+1):4*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = ( n - (3*imw*imh)-imh ).*( ((n - (3*imw*imh)) >imh)&((n - (3*imw*imh)) <(imw-1)*imh+1) )+1*(~( ((n - (3*imw*imh)) >imh)&((n - (3*imw*imh)) <(imw-1)*imh+1) ));
+v(n) = -1 * ( ((n - (3*imw*imh)) >imh)&((n - (3*imw*imh)) <(imw-1)*imh+1) );
+% right
+n = (4*imh*imw+1):5*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = ( n - (4*imw*imh)+imh ).*( ((n - (4*imw*imh)) < ((imw-1)*imh+1))&((n - (4*imw*imh)) > imh) )+1*(~( ((n - (4*imw*imh)) < ((imw-1)*imh+1))&((n - (4*imw*imh)) > imh) ));
+v(n) = -1 * ( ((n - (4*imw*imh)) < ((imw-1)*imh+1))&((n - (4*imw*imh)) > imh) );
 
 A = sparse(i,j,v);
 b = A*(reshape(im_s(:,:,1),[imh*imw,1]));
@@ -143,71 +85,34 @@ i = zeros(1);
 j = zeros(1);
 v = zeros(1);
 
-counter = 1;
-row = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if n==1 || n==imw || m==1 || m== imh
-            i(row) = counter;
-            j(row) = (n-1)*imh+m;
-            v(row) = 1;
-        else
-            i(row) = counter;
-            j(row) = (n-1)*imh+m;
-            v(row) = 4; % *( mask_s( (n-1)*imh+m )==1 )
-        end
-        counter = counter + 1;
-        row = row + 1;
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if mask_s( (n-1)*imh+m )  == 1
-            i(row) = counter;
-            j(row) = (n-1)*imh+m+1;
-            v(row) = -1; % *( mask_s( (n-1)*imh+m+1 )==1 )
-            row = row + 1;
-        end
-        counter = counter + 1;
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if mask_s( (n-1)*imh+m )  == 1
-            i(row) = counter;
-            j(row) = (n-1)*imh+m-1;
-            v(row) = -1; %*( mask_s( (n-1)*imh+m-1 )==1 )
-            row = row + 1;
-        end
-        counter = counter + 1;
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if mask_s( (n-1)*imh+m )  == 1
-            i(row) = counter;
-            j(row) = (n-2)*imh+m;
-            v(row) = -1; % *( mask_s( (n-2)*imh+m )==1 )
-            row = row + 1;
-        end
-        counter = counter + 1;
-    end
-end
-counter = 1;
-for n = 1:(imw)
-    for m = 1:(imh)
-        if mask_s( (n-1)*imh+m )  == 1
-            i(row) = counter;
-            j(row) = (n)*imh+m;
-            v(row) = -1; % *( mask_s( (n)*imh+m )==1 )
-            row = row + 1;
-        end
-        counter = counter + 1;
-    end
-end
+% Inner
+n = 1:imh*imw;
+i(n) = 1:imw*imh;
+j(n) = 1:imw*imh;
+v(n) = 1 + 1*( (1<n)&(n<imh) | ((imw-1)*imh<n)&(n<imw*imh) | (mod(n,imh)==1&n~=1&n~=((imw-1)*imh+1)) | mod(n,imh)==0&n~=imh&n~=(imw*imh) )  ...
+    + 3*( ~( (1<=n)&(n<=imh) | ((imw-1)*imh<=n)&(n<=imw*imh) | (mod(n,imh)==1&n~=1&n~=((imw-1)*imh+1)) | mod(n,imh)==0&n~=imh&n~=(imw*imh) )&( mask_s( n )==1 ) ) ;
+% % top
+n = (imh*imw+1):2*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = (n - (imw*imh)-1 ).*( (1<mod(n - (imw*imh),imh))&(mask_s( n - (imw*imh) )  == 1) )+1*(~( (1<mod(n - (imw*imh),imh))&(mask_s( n - (imw*imh) )  == 1) ));
+v(n) = -1 * ( (1<mod(n - (imw*imh),imh))&( mask_s( (n - (imw*imh)-1).*(logical(n - (imw*imh)-1)) + 1*(~logical(n - (imw*imh)-1))  )==1 ) ) ;
+% bottom
+n = (2*imh*imw+1):3*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = (n - (2*imw*imh)+1 ).*( (1<mod(n - (2*imw*imh),imh))&(mask_s( n - 2*(imw*imh) )  == 1) )+1*(~( (1<mod(n - (2*imw*imh),imh))&(mask_s( n - 2*(imw*imh) )  == 1) ));
+v(n) = -1 * ( (1<mod(n - (2*imw*imh),imh))&( mask_s( (n - (2*imw*imh)+1).*(logical(n-3*imh*imw)) + 1*(~logical(n-3*imw*imh)) )==1 ) );
+% left
+n = (3*imh*imw+1):4*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = ( n - (3*imw*imh)-imh ).*( ((n - (3*imw*imh)) >imh)&((n - (3*imw*imh)) <(imw-1)*imh+1)&(mask_s( n - 3*(imw*imh) )  == 1) )+1*(~( ((n - (3*imw*imh)) >imh)&((n - (3*imw*imh)) <(imw-1)*imh+1)&(mask_s( n - 3*(imw*imh) )  == 1) ));
+v(n) = -1 * ( ((n - (3*imw*imh)) >imh)&((n - (3*imw*imh)) <(imw-1)*imh+1)&( mask_s( (n - 3*imw*imh-imh).*( (n-3*imh*imw-imh)>0 ) + 1*(~( (n-3*imh*imw-imh)>0 )) )==1 ) );
+% right
+n = (4*imh*imw+1):5*imh*imw;
+i(n) = 1:imh*imw;
+j(n) = ( n - (4*imw*imh)+imh ).*( ((n - (4*imw*imh)) < ((imw-1)*imh+1))&((n - (4*imw*imh)) > imh)&(mask_s( n - 4*(imw*imh) )  == 1) )+1*(~( ((n - (4*imw*imh)) < ((imw-1)*imh+1))&((n - (4*imw*imh)) > imh)&(mask_s( n - 4*(imw*imh) )  == 1) ));
+v(n) = -1 * ( ((n - (4*imw*imh)) < ((imw-1)*imh+1))&((n - (4*imw*imh)) > imh)&( mask_s( (n -4*imw*imh+imh).*( (n -4*imw*imh+imh)<imw*imh+1 ) + 1*(~((n -4*imw*imh+imh)<imw*imh+1)) )==1 ) );
+
+
 
 A2 = sparse(i,j,v);
 
@@ -219,8 +124,8 @@ A2 = sparse(i,j,v);
 %TODO: solve the equation
 %use "lscov" or "\", please google the matlab documents
 solution = A2\b;
-% error = sum(abs(A*solution-b));
-% disp(error)
+error = sum(abs(A2*solution-b));
+disp(error)
 imgout = solution;
 %TODO: copy those variable pixels to the appropriate positions
 %      in the output image to obtain the blended image
